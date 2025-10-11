9d1ae8a917d2e208c3114ec65c9da9496040823a
Daryl  Tchienang <daryltchienang@MacBookPro.fritz.box>
2025-10-11 15:27:40 +0200

    fix: 404 requests

--- tchapia/tchapia/urls.py
+++ tchapia/tchapia/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.defaults import bad_request, permission_denied, page_not_found, server_error
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +31,9 @@ urlpatterns = [
 # Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Custom error handlers
+handler400 = 'django.views.defaults.bad_request'
+handler403 = 'django.views.defaults.permission_denied'
+handler404 = 'django.views.defaults.page_not_found'
+handler500 = 'django.views.defaults.server_error'
